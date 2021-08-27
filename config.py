7d220a072b075b0fab7712c6a209ec8979c61b20
Necan03 <89589713+Necan03@users.noreply.github.com>
2021-08-28 05:09:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = os.environ.get("1965998576:AAEe0U0mIrl21Di1-GXbiv8gWo-czYFZwxA")
 APP_ID = int(os.environ.get("7024783z"))
 API_HASH = os.environ.get("946669c3-8665-4f01-8662-a4aeaffaeb40")
 
-youtube_next_fetch = 0  # time in minute
+youtube_next_fetch = 5  # time in minute
 
 
 EDIT_TIME = 5
